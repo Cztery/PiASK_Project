0e73e3b4c0fa14fcfc602775905c6c6dd59fbc94
Marcin Kurkowski <plgmkurkow@zeus.cyfronet.pl>
2021-01-21 13:20:37 +0100

    created a makefile and added sbatch scripts for mpi and mp

---
+++ integral_cu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH -J first_gpu_job
+## Get one node, one CPU-GPU pair
+#SBATCH -N 1
+#SBATCH --partition=plgrid-gpu
+#SBATCH --reservation=piask_gpu
+#SBATCH --gres=gpu:1
+## Specify exec time, queue and output
+#SBATCH --time=00:10:00
+#SBATCH --partition=plgrid-gpu
+#SBATCH --output=cu_integral.out
+## Select module and run task
+cd $HOME/labwork/PIASK_Project
+./cu_integral

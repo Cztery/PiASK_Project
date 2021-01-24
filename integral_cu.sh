8ec6d8e2a758723d5d3f1a421de9c9598910aac8
Jakub Brzezowski <jakubbrzez@wp.pl>
2021-01-24 21:02:55 +0100

    Confict fix

--- integral_cu.sh
+++ integral_cu.sh
@@ -8,14 +8,7 @@
 ## Specify exec time, queue and output
 #SBATCH --time=00:10:00
 #SBATCH --partition=plgrid-gpu
-<<<<<<< HEAD:integral.sh
-#SBATCH --output=out_integral_gpu
-## Select module and run task
-cd $HOME/labwork/PIASK_Project
-./integral_out
-=======
 #SBATCH --output=cu_integral.out
 ## Select module and run task
 cd $HOME/labwork/PiASK_Project
 ./cu_integral
->>>>>>> 0f86fb1ea2fa3f9769781e96ce830d801e58a982:integral_cu.sh
